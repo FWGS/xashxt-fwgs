14c04d40e83fa9be7a9675d5bbf52c205cf9361e
mittorn <mittorn@sibmail.com>
2021-02-13 15:34:04 +0300

    pmove: workaround calling conversion difference on E2K

--- common/pmtrace.h
+++ common/pmtrace.h
@@ -24,6 +24,35 @@ typedef struct
 
 typedef struct pmtrace_s pmtrace_t;
 
+#ifdef __cplusplus
+// workaround layout difference between Vector and float[3]
+// when aggregate-returing pmtrace_s, it comes with float[3], not Vector class
+// which differs in calling conversion on E2K
+
+typedef struct
+{
+	float	normal[3];
+	float	dist;
+} pmplane_c;
+
+typedef struct pmtrace_c_s
+{
+	qboolean	allsolid;		// if true, plane is not valid
+	qboolean	startsolid;	// if true, the initial point was in a solid area
+	qboolean	inopen, inwater;  // End point is in empty space or in water
+	float	fraction;		// time completed, 1.0 = didn't hit anything
+	float	endpos[3];		// final position
+	pmplane_c	plane;		// surface normal at impact
+	int	ent;		// entity at impact
+	float	deltavelocity[3];	// Change in player's velocity caused by impact.  
+				// Only run on server.
+	int	hitgroup;
+} pmtrace_c;
+#include <string.h>
+#else
+#define pmtrace_c pmtrace_t
+#endif
+
 struct pmtrace_s
 {
 	qboolean	allsolid;		// if true, plane is not valid
@@ -36,6 +65,13 @@ struct pmtrace_s
 	vec3_t	deltavelocity;	// Change in player's velocity caused by impact.  
 				// Only run on server.
 	int	hitgroup;
+#ifdef __cplusplus
+	pmtrace_s & operator = (const pmtrace_c_s &other)
+	{
+		memcpy(this, &other, sizeof(*this));
+		return *this;
+	}
+#endif
 };
 
 #endif//PM_TRACE_H
--- game_shared/pm_defs.h
+++ game_shared/pm_defs.h
@@ -203,7 +203,7 @@ typedef struct playermove_s
 	int		(*PM_PointContents)( float *p, int *truecontents /*filled in if this is non-null*/ );
 	int		(*PM_TruePointContents)( float *p );
 	int		(*PM_HullPointContents)( struct hull_s *hull, int num, float *p );   
-	pmtrace_t		(*PM_PlayerTrace)( float *start, float *end, int traceFlags, int ignore_pe );
+	pmtrace_c		(*PM_PlayerTrace)( float *start, float *end, int traceFlags, int ignore_pe );
 	struct pmtrace_s	*(*PM_TraceLine)( float *start, float *end, int flags, int usehulll, int ignore_pe );
 	long		(*RandomLong)( long lLow, long lHigh );
 	float		(*RandomFloat)( float flLow, float flHigh );
